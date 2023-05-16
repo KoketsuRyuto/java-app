d2c0c984f18f400d62e081d7d363070b248ff8f0
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-05-16 22:21:59 +0900

    [firstcommit]

---
+++ src/test/java/sport/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sport.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
